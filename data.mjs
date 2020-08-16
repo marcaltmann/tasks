c376e7e8c26707499a5caea2d6ef6e5939a8f199
Marc Altmann <marcaltmann@posteo.de>
2020-08-16 08:14:20 +0200

    Add views and fake data

---
+++ data.mjs
@@ -0,0 +1,8 @@
+let tasks = [
+  {
+    id: 0,
+    name: 'Call agent and ask about new jobs/projects',
+  },
+];
+
+export { tasks };
--- index.mjs
+++ index.mjs
@@ -1,12 +1,18 @@
 import express from 'express';
+import { tasks } from './data.mjs';
 
 let port = process.env.PORT || 3000;
 let app = express();
 
+app.set('view engine', 'pug');
+
 app.get('/', (req, res) => {
-  res.end('Hello!');
+  res.render('index', {
+    tasks: tasks,
+    title: 'Tasks',
+  });
 });
 
-app.listen(port);
-
-console.log('Hello world');
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}.`);
+});
