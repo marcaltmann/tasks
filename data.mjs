bb26e84d39eec2d12f09109e6507276919a816ff
Marc Altmann <marcaltmann@posteo.de>
2020-08-17 19:33:54 +0200

    Add more routes

--- data.mjs
+++ data.mjs
@@ -1,8 +1,22 @@
-let tasks = [
+const tasks = [
   {
     id: 0,
     name: 'Call agent and ask about new jobs/projects',
   },
 ];
 
-export { tasks };
+const projects = [
+  {
+    id: 0,
+    name: 'Find new job/project',
+  },
+];
+
+const categories = [
+  {
+    id: 0,
+    name: 'Communicate',
+  },
+];
+
+export { tasks, projects, categories };
--- index.mjs
+++ index.mjs
@@ -1,16 +1,34 @@
 import express from 'express';
-import { tasks } from './data.mjs';
+import { tasks, projects, categories } from './data.mjs';
 
 let port = process.env.PORT || 3000;
 let app = express();
 
 app.set('view engine', 'pug');
 
-app.get('/', (req, res) => {
-  res.render('index', {
-    tasks: tasks,
+app.get('/tasks', (req, res) => {
+  res.render('tasks', {
+    tasks,
     title: 'Tasks',
   });
+});
+
+app.get('/projects', (req, res) => {
+  res.render('projects', {
+    projects,
+    title: 'Projects',
+  });
+});
+
+app.get('/categories', (req, res) => {
+  res.render('categories', {
+    categories,
+    title: 'Categories',
+  });
+});
+
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Welcome' });
 });
 
 app.listen(port, () => {
