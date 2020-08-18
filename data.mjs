51a86f33270e50c18f60cf41e2f9ca1ffa61a928
Marc Altmann <marcaltmann@posteo.de>
2020-08-18 10:19:45 +0200

    Use more data for compression to kick in

--- data.mjs
+++ data.mjs
@@ -1,6 +1,44 @@
+const animal = 'alligator';
+
 const tasks = [
   {
     id: 0,
+    name: animal.repeat(1000),
+  },
+  {
+    id: 1,
+    name: 'Call agent and ask about new jobs/projects',
+  },
+  {
+    id: 2,
+    name: 'Call agent and ask about new jobs/projects',
+  },
+  {
+    id: 3,
+    name: 'Call agent and ask about new jobs/projects',
+  },
+  {
+    id: 4,
+    name: 'Call agent and ask about new jobs/projects',
+  },
+  {
+    id: 5,
+    name: 'Call agent and ask about new jobs/projects',
+  },
+  {
+    id: 6,
+    name: 'Call agent and ask about new jobs/projects',
+  },
+  {
+    id: 7,
+    name: 'Call agent and ask about new jobs/projects',
+  },
+  {
+    id: 8,
+    name: 'Call agent and ask about new jobs/projects',
+  },
+  {
+    id: 9,
     name: 'Call agent and ask about new jobs/projects',
   },
 ];
