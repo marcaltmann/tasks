01b5d6c5ea194dab6b2267eb610520d88355bbff
Marc Altmann <marcaltmann@posteo.de>
2020-08-18 10:03:26 +0200

    Try out compression middleware

--- index.mjs
+++ index.mjs
@@ -1,8 +1,11 @@
+import compression from 'compression';
 import express from 'express';
 import router from './router.mjs';
 
 let port = process.env.PORT || 3000;
 let app = express();
+
+app.use(compression());
 
 app.set('view engine', 'pug');
 
