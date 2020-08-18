f80470d16f4cb8a068dcbdf3aa2dcbeacefc6d06
Marc Altmann <marcaltmann@posteo.de>
2020-08-18 11:55:55 +0200

    Install React

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('Hello world!');
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+const e = React.createElement;
+
+class HelloWorld extends Component {
+  render() {
+    return e(
+      'h1',
+      null,
+      'Hello world!'
+    );
+  }
+}
+
+const domContainer = document.querySelector('#app_container');
+ReactDOM.render(e(HelloWorld), domContainer);
