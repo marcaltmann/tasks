31ef0b83008aaf4cdb0b1869f40900c354280b32
Marc Altmann <marcaltmann@posteo.de>
2020-08-25 15:19:04 +0200

    Configure eslint

--- src/action_creators.js
+++ src/action_creators.js
@@ -1,5 +1,7 @@
 import { createAction } from '@reduxjs/toolkit';
 
+let x;
+
 export const requestCategoriesFetch = createAction('categories/requestFetch');
 export const resolveCategoriesFetch = createAction('categories/resolveFetch');
 export const rejectCategoriesFetch = createAction('categories/rejectFetch');
