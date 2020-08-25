87dbcb83e6b4cd9694fc564ba35fd6040213d0ed
Marc Altmann <marcaltmann@posteo.de>
2020-08-25 16:13:25 +0200

    Add more tests

--- src/categories_slice.test.js
+++ src/categories_slice.test.js
@@ -1,9 +1,37 @@
 import slice from './categories_slice.js';
 
-test('reducer', () => {
-  const { reducer, actions } = slice;
-  const { requestFetch, resolveFetch, rejectFetch } = actions;
+const { reducer, actions } = slice;
+const { requestFetch, resolveFetch, rejectFetch } = actions;
 
+test('requestFetch does nothing', () => {
+  let result = reducer(null, requestFetch());
+  let expected = null;
+
+  expect(result).toBe(expected);
+});
+
+test('resolveFetch saves fetched results', () => {
+  let result = reducer(null, resolveFetch({
+    entities: {
+      categories: {
+        0: { id: '0' },
+        1: { id: '1' },
+      },
+    },
+    result: ['0', '1'],
+  }));
+  let expected = {
+    byId: {
+      0: { id: '0' },
+      1: { id: '1' },
+    },
+    allIds: ['0', '1'],
+  };
+
+  expect(result).toEqual(expected);
+});
+
+test('rejectFetch registers error', () => {
   let result = reducer(null, rejectFetch('Error'));
   let expected = { error: 'Error' };
 
